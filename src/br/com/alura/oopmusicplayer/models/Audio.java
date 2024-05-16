e73dca1a3a6c842b4dfa95dc27af0830a6266d87
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-16 11:46:31 -0300

    Add Audio superclass with basic fields and methods for subclasses.

---
+++ src/br/com/alura/oopmusicplayer/models/Audio.java
@@ -0,0 +1,36 @@
+package br.com.alura.oopmusicplayer.models;
+
+public class Audio {
+    private String  title;
+    private int     totalPlays;
+    private int     totalLikes;
+    private int     rating;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getTotalPlays() {
+        return totalPlays;
+    }
+
+    public int getTotalLikes() {
+        return totalLikes;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void like() {
+        this.totalLikes++;
+    }
+
+    public void setTotalPlays() {
+        this.totalPlays++;
+    }
+}
