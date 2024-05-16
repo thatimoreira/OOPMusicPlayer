b06eae42e9bc74cd61bc5b0fb6b417ba4ae6ab49
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-16 13:05:22 -0300

    Add Main class to demonstrate the usage of Music and Podcast classes. Simulate plays and likes for a Music instance and a Podcast instance.

---
+++ src/br/com/alura/oopmusicplayer/main/Main.java
@@ -0,0 +1,39 @@
+package br.com.alura.oopmusicplayer.main;
+
+import br.com.alura.oopmusicplayer.models.Music;
+import br.com.alura.oopmusicplayer.models.Podcast;
+
+public class Main {
+    public static void main(String[] args) {
+        Music   myMusic = new Music();
+        myMusic.setTitle("Video");
+        myMusic.setArtist("India.Arie");
+        myMusic.setAlbum("Acoustic Soul");
+        myMusic.setGenre("Soul");
+
+        // simulating the plays
+        for (int i = 0; i < 7191; i ++){
+            myMusic.play();
+        }
+
+        // simulating the likes
+        for (int i = 0; i < 6972; i++){
+            myMusic.like();
+        }
+
+        Podcast myPodcast = new Podcast();
+        myPodcast.setTitle("Hipsters Ponto Tech");
+        myPodcast.setHost("Paulo Silveira");
+        myPodcast.setDecsription("Discussion about technology, programming, design, startups, and the latest trends.");
+
+        // simulating the plays
+        for (int i = 0; i < 2001; i ++){
+            myPodcast.play();
+        }
+
+        // simulating the likes
+        for (int i = 0; i < 1961; i++){
+            myMusic.like();
+        }
+    }
+}
